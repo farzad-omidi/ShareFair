af4caa434c08af840564e3f6a5015b4df9b889f7
Farzad <frzdomidi@gmail.com>
2026-07-11 10:51:56 +0200

    Revert "Rebuild ShareFair as a full-stack Next.js + Supabase app"

--- src/components/Toast.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useSpace } from "@/lib/store";
-
-export function Toast() {
-  const { toast } = useSpace();
-  return <div className={`toast${toast ? " show" : ""}`}>{toast}</div>;
-}
